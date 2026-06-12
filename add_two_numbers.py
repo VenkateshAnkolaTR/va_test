53d32a746a7a69c2146f6e08c1779c0f8db43669
Venkatesh <Venkatesh.Ankola@thomsonreuters.com>
2026-06-12 14:24:18 +0530

    Code

---
+++ add_two_numbers.py
@@ -0,0 +1,8 @@
+def add_two_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print(f"The sum is: {add_two_numbers(num1, num2)}")
